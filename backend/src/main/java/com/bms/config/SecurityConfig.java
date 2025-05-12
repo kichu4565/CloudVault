18f2812e91c5f09666d91573306e4ac60d5fc079
Kishor <kruz4565@gmail.com>
2025-05-12 12:13:00 +0530

    Update SecurityConfig.java
    Recent changes by kishor

--- backend/src/main/java/com/bms/config/SecurityConfig.java
+++ backend/src/main/java/com/bms/config/SecurityConfig.java
@@ -64,12 +64,12 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
-    } 
+    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:63139")); // Frontend URLs
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "*"));
         configuration.setAllowCredentials(true);
